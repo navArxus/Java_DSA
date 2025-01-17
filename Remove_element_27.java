38185246d7328c1857170aa09dd8ac198a93eee8
Nav <108321979+navArxus@users.noreply.github.com>
2025-01-17 22:50:55 +0530

    day-2 p-2

--- Remove_element_27.java
+++ Remove_element_27.java
@@ -1,5 +1,40 @@
 public class Remove_element_27 {
+
+    static int replaceIndex(int[] arr, int index, int val) {
+        for (int i = index; i < arr.length; i++) {
+            if (arr[i] != val) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    static int removeElement(int[] nums, int val) {
+        int index = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == val) {
+                int newindex = replaceIndex(nums, i, val);
+                if (newindex != -1) {
+                    int temp = nums[i];
+                    nums[i] = nums[newindex];
+                    nums[newindex] = temp;
+                    index++;
+                }
+            } else {
+                index++;
+            }
+        }
+
+        return index;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int[] nums = { 3,2,2,3 };
+        int val = 3;
+        System.out.println(removeElement(nums, val));
+        for (int i : nums) {
+            System.out.print(i);
+        }
     }
 }
